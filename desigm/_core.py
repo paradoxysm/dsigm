d27d97588f950979c9a088c0d215551a3ee16b9d
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-02-09 03:11:39 -0500

    Add init validation

--- desigm/_core.py
+++ desigm/_core.py
@@ -4,6 +4,9 @@
 # License: BSD 3 clause
 
 from scipy.stats import multivariate_normal as mvn
+import numpy as np
+
+from _utils import formatArray, checkDimensionality
 
 class Core:
 	"""
@@ -26,10 +29,25 @@ class Core:
 		The parent CoreCluster this Core is associated with.
 	"""
 	def __init__(self, mu=[0], sigma=[1], delta=[1], cluster=None):
-		self.mu = mu
-		self.sigma = sigma
-		self.delta = delta
+		self.dim = checkDimensionality(mu, sigma)
+		self.mu = np.asarray(mu)
+		self.sigma = np.asarray(sigma)
+		self.delta = np.asarray(delta)
 		self.cluster = cluster
+		self._validate_init()
+
+	def _validate_init(self):
+		"""
+		Validate the argument types for __init__
+		"""
+		if np.isscalar(self.mu):
+			raise ValueError("Invalid argument provided for mu. Must be a vector")
+		if np.isscalar(self.sigma):
+			raise ValueError("Invalid argument provided for sigma. Must be a vector")
+		if np.isscalar(self.delta):
+			raise ValueError("Invalid argument provided for delta. Must be a vector")
+		if not isinstance(self.cluster, (None, CoreCluster)):
+			raise ValueError("Invalid argument provided for cluster. Must be None or CoreCluster. Found " + type(self.cluster).__name__)
 
 	def pdf(self, data):
 		"""
@@ -45,6 +63,8 @@ class Core:
         pdf : ndarray or scalar
             Probability density function evaluated at `datas`
 		"""
+		data = formatArray(data)
+		checkDimensionality(data, self.mu, self.sigma)
 		return mvn.pdf(x=data, mean=self.mu, cov=self.sigma)
 
 
@@ -64,6 +84,26 @@ class CoreCluster:
 		A list of CoreCluster children in a hierarchical manner.
 	"""
 	def __init__(self, cores=[], parent=None, children=[]):
-		self.cores = []
+		self.cores = np.asarray(cores)
 		self.parent = parent
-		self.children = []
+		self.children = np.asarray(children)
+		self._validate_init()
+
+	def _validate_init(self):
+		"""
+		Validate the argument types for __init__
+		"""
+		if np.isscalar(self.cores):
+			raise ValueError("Invalid argument provided for cores. Must be a list of Cores")
+		core_types = set([type(item) for item in self.cores])
+		if len(core_types) != 1 or Core not in core_types:
+			raise ValueError("Invalid argument provided for cores. Must be a list of Cores")
+
+		if not isinstance(self.parent, (None, CoreCluster)):
+			raise ValueError("Invalid argument provided for parent. Must be None or CoreCluster. Found " + type(self.parent).__name__)
+
+		if np.isscalar(self.children):
+			raise ValueError("Invalid argument provided for children. Must be a list of CoreClusters")
+		children_types = set([type(item) for item in self.children])
+		if len(children_types) != 1 or CoreCluster not in children_types:
+			raise ValueError("Invalid argument provided for children. Must be a list of CoreClusters")
