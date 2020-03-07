6ab89157f05e681f4a8fc4c88d6c31bfc9764e0a
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-03-06 22:07:11 -0500

    Implement test for _validate_data

---
+++ tests/test_sgmm.py
@@ -0,0 +1,26 @@
+import pytest
+import numpy as np
+
+from dsigm import SGMM
+
+"""
+Test
+----
+SGMM
+"""
+
+@pytest.mark.parametrize("data, exp, dim_exp", [
+	([0], np.asarray([[0.]]), 1),
+	([[0]], np.asarray([[0.]]), 1),
+	([[[0]]], np.asarray([[0.]]), 1),
+	([[3,4,5]], np.asarray([[3.,4.,5.]]), 3),
+	([[[3],[4],[5]]], np.asarray([[3.],[4.],[5.]]), 1),
+	([[3,4],[5,6]], np.asarray([[3.,4.],[5.,6.]]), 2),
+	([1,2,34], np.asarray([[1.],[2.],[34.]]), 1),
+	([[[1,2],[3,4]]], np.asarray([[1.,2.],[3.,4.]]), 2),
+])
+
+def test_validate_data(data, exp, dim_exp):
+	sgmm = SGMM()
+	assert np.all(sgmm._validate_data(data) == exp)
+	assert sgmm.dim == dim_exp
